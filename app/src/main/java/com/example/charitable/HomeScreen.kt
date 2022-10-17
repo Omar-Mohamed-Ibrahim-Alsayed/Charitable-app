d45e2c9fd2e0bef30513969705edaa3d7f3f11c9
Omar-Mohamed-Ibrahim-Alsayed <93681796+Omar-Mohamed-Ibrahim-Alsayed@users.noreply.github.com>
2022-10-17 10:53:41 +0200

    prototype2.0

--- app/src/main/java/com/example/charitable/HomeScreen.kt
+++ app/src/main/java/com/example/charitable/HomeScreen.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 class HomeScreen : Fragment() {
+
+    lateinit var donate: View
+    lateinit var participate: View
+    lateinit var establishment: View
+    lateinit var callUs: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +20,37 @@ class HomeScreen : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        donate = view.findViewById<ImageView>(R.id.donateBtn)
+        participate = view.findViewById<ImageView>(R.id.partBtn)
+        establishment = view.findViewById<ImageView>(R.id.estBtn)
+        callUs = view.findViewById<ImageView>(R.id.callBtn)
+
+        donate.setOnClickListener{
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, Donation())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        participate.setOnClickListener{
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, Activity())
+                addToBackStack(null)
+                commit()
+            }
+        }
+        establishment.setOnClickListener{
+            parentFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, Establishments())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
     }
 
 }
